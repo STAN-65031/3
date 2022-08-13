e86a8b39f186c7300a9a1d199be992f42f8c02cf
STAN-65031 <lox.ibany@gmail.com>
2022-08-13 22:09:10 +0300

    усталл...

---
+++ homeWork3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(CubeNumber(N));
+
+string CubeNumber(int number)
+ {
+   string result = "";
+    for(int i = 1; i <= number; i++)
+    {  
+        result += $"{Math.Pow(i, 3)} ";
+ }
+   
+   return result;
+ }
